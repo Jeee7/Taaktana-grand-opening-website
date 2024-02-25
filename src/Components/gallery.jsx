76216b860d25fb694814a1d509abb9a55112d103
Jeee7 <arya.ziddan7@gmail.com>
2024-02-25 15:12:38 +0700

    gallery-error

--- src/Components/gallery.jsx
+++ src/Components/gallery.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import {React, useState} from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import '../Assets/Style/Gallery.css'
+import {SlideshowLightbox} from 'lightbox.js-react'
+import 'lightbox.js-react/dist/index.css'
 import gallery1 from "../Assets/Img/Gallery Foto/Gallery 01.png"
 import gallery2 from "../Assets/Img/Gallery Foto/Gallery 02.png"
 import gallery3 from "../Assets/Img/Gallery Foto/Gallery 03.png"
@@ -12,42 +14,13 @@ import gallery8 from "../Assets/Img/Gallery Foto/Gallery 08.png"
 import gallery9 from "../Assets/Img/Gallery Foto/Gallery 09.png"
 
 const Gallery = () => {
+    
   return (
     <div className="photos-gallery">
         <Container className="d-flex justify-content-center">
-        <Row style={{display: 'flex'}}>
-            <Col>
-            <img src={gallery1} alt="Photo 1" className="photos" />
-            </Col>
-            <Col>
-            <img src={gallery2} alt="Photo 1" className="photos" />
-            </Col>
-            <Col>
-            <img src={gallery3} alt="Photo 1" className="photos" />
-            </Col>
-        </Row>
-        <Row style={{display: 'flex'}}>
-            <Col>
-            <img src={gallery4} alt="Photo 1" className="photos" />
-            </Col>
-            <Col>
-            <img src={gallery5} alt="Photo 1" className="photos" />
-            </Col>
-            <Col>
-            <img src={gallery6} alt="Photo 1" className="photos" />
-            </Col>
-        </Row>
-        <Row style={{display: 'flex'}}>
-            <Col>
-            <img src={gallery7} alt="Photo 1" className="photos" />
-            </Col>
-            <Col>
-            <img src={gallery8} alt="Photo 1" className="photos" />
-            </Col>
-            <Col>
-            <img src={gallery9} alt="Photo 1" className="photos" />
-            </Col>
-        </Row>
+            <SlideshowLightbox className="container grid grid-cols-3 gap-2 mx-auto">
+                <img className="w-full rounded" src={gallery1} />
+            </SlideshowLightbox> 
         </Container>
     </div>
   );
